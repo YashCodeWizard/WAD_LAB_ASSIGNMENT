f5ec4eb5e33e4aeddc31fdafd34fd0157bc66a0e
YashCodeWizard <161604828+YashCodeWizard@users.noreply.github.com>
2025-03-17 12:16:38 +0530

    Add files via upload
    updated files

---
+++ Angular/angu_project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './component/registration/registration.component';
+import { LoginComponent } from './component/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
